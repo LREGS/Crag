1b310c2adcf2754ba3e75d2942cee58896aaf25a
William Cullum <william-cullum@hotmail.co.uk>
2024-02-25 12:03:00 +0000

    tests, added server struct and store interface

---
+++ main.go
@@ -0,0 +1,41 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+	"strings"
+)
+
+type CragStore interface {
+	GetForecast(name string) string
+}
+
+type CragServer struct {
+	store CragStore
+}
+
+func (c CragServer) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	crag := strings.TrimPrefix(r.URL.Path, "/crags/")
+	fmt.Fprint(w, c.store.GetForecast(crag))
+
+}
+
+func GetForecast(crag string) string {
+	if crag == "stanage" {
+		return "cold"
+	}
+
+	if crag == "milestone" {
+		return "dry"
+	}
+	return ""
+
+}
+
+func main() {
+
+	server := &CragServer{}
+
+	log.Fatal(http.ListenAndServe(":6969", server))
+}
--- server_test.go
+++ server_test.go
@@ -1,23 +1,46 @@
-package server
+package main
 
 import (
+	"fmt"
 	"net/http"
 	"net/http/httptest"
 	"testing"
 )
 
 func TestGETCrags(t *testing.T) {
-	t.Run("returns forecast of a crag", func(t *testing.T) {
-		request, _ := http.NewRequest(http.MethodGet, "/crags/forecast", nil)
+
+	server := &CragServer{}
+
+	t.Run("returns forecast of a stange", func(t *testing.T) {
+		request := newGetForecastRequest("stanage")
 		response := httptest.NewRecorder()
 
-		CragServer(response, request)
+		server.ServeHTTP(response, request)
 
-		got := response.Body.String()
-		want := "cold"
+		assertBodyResponse(t, response.Body.String(), "cold")
 
-		if got != want {
-			t.Errorf("got %q, want %q", got, want)
-		}
 	})
+
+	t.Run("returns forecast of milestone", func(t *testing.T) {
+		request := newGetForecastRequest("milestone")
+		response := httptest.NewRecorder()
+
+		server.ServeHTTP(response, request)
+
+		assertBodyResponse(t, response.Body.String(), "dry")
+
+	})
+}
+
+func newGetForecastRequest(name string) *http.Request {
+	req, _ := http.NewRequest(http.MethodGet, fmt.Sprintf("/crags/%s", name), nil)
+	return req
+
+}
+
+func assertBodyResponse(t testing.TB, got, want string) {
+	t.Helper()
+	if got != want {
+		t.Errorf("response body is wrong, got %q and wanted %q", got, want)
+	}
 }
