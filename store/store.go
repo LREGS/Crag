c7c3853d6b8a3fb4afde5dd43bd77cd04e980c51
William Cullum <william-cullum@hotmail.co.uk>
2024-03-05 19:28:29 +0000

    defined tests as db tests instead as they are not actually testing the store and are more of a pre-requisite to testing the store. Stil not sure about ttd, and maybe I just write my tests after my code?! I dunno I do see the benefit of writing small chunks of code but sometimes I dont know wwhat im really doing so its hard to break it down into small simple chunks

--- store/store.go
+++ store/store.go
@@ -1,7 +1,45 @@
 package store
 
-import "database/sql"
+import (
+	"database/sql"
+	"errors"
+	"fmt"
+
+	"github.com/lregs/Crag/env"
+)
 
 type SqlStore struct {
 	masterX *sql.DB
+	Stores  SqlStoreStores
+}
+
+type SqlStoreStores struct {
+	Crag CragStore
+}
+
+func NewSqlStore() (*SqlStore, error) {
+	store := &SqlStore{}
+	err := store.initConnection()
+	if err != nil {
+		return nil, errors.New("error getting connection")
+	}
+
+	return store, nil
+}
+
+func (ss SqlStore) initConnection() error {
+	DBURL, err := env.DBString()
+	if err != nil {
+		fmt.Printf("error establishing db connection %s", err)
+	}
+
+	ss.masterX, err = sql.Open("postgres", DBURL)
+	if err != nil {
+		panic(err)
+	}
+	return err
+}
+
+func (ss *SqlStore) GetMasterX() *sql.DB {
+	return ss.masterX
 }
