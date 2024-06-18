6f148679b458020c9f458c91b1e67493a2772922
william <william-cullum@hotmail.co.uk>
2024-06-18 21:01:44 +0100

    mostly reading on html

--- Crag/main.go
+++ Crag/main.go
@@ -43,10 +43,6 @@ func main() {
 		log.Printf("db connection failed %s", err)
 	}
 
-	if err := initdb(log, conn); err != nil {
-		panic(err)
-	}
-
 	store, err := store.NewSqlStore(&store.StoreConfig{DbConnection: conn})
 	if err != nil {
 		log.Fatalf("Could not create store because of error: %s", err)
@@ -68,56 +64,4 @@ func NewLogger(filename string) *log.Logger {
 		panic("bad file")
 	}
 	return log.New(logfile, "[main]", log.Ldate|log.Ltime|log.Lshortfile)
-}
-
-// I just need to edit the posgres docker image to include the execution of this on start-up or init - maybe
-
-var initDb = `DROP TABLE IF EXISTS forecast;
-DROP TABLE IF EXISTS report;
-DROP TABLE IF EXISTS climb;
-DROP TABLE IF EXISTS crag;
-
--- Create tables
-CREATE TABLE crag (
-	Id SERIAL PRIMARY KEY, 
-	Name TEXT UNIQUE, 
-	Latitude DOUBLE PRECISION,
-	Longitude DOUBLE PRECISION
-);
-
-CREATE TABLE climb (
-	Id SERIAL PRIMARY KEY,
-	Name VARCHAR(255) UNIQUE,
-	Grade VARCHAR(255),
-	CragID INTEGER REFERENCES crag(Id)
-);
-
-CREATE TABLE report (
-	Id SERIAL PRIMARY KEY, 
-	Content VARCHAR(255),
-	Author VARCHAR(255),
-	CragID INTEGER REFERENCES crag(Id)
-);
-
-CREATE TABLE forecast (
-	Id Int, 
-	Time VARCHAR(255) UNIQUE,
-	ScreenTemperature DOUBLE PRECISION,
-	FeelsLikeTemp DOUBLE PRECISION, 
-	WindSpeed DOUBLE PRECISION,
-	WindDirection DOUBLE PRECISION,
-	totalPrecipitation DOUBLE PRECISION,
-	ProbofPrecipitation INT,
-	Latitude DOUBLE PRECISION,
-	Longitude DOUBLE PRECISION
-);`
-
-func initdb(log *log.Logger, db *pgxpool.Pool) error {
-	if _, err := db.Exec(context.Background(), initDb); err != nil {
-		log.Printf("init db failed %s", err)
-	}
-
-	log.Printf("db inited")
-	return nil
-
 }
