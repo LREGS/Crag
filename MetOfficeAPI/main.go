525fcc1688128cd5f6c6d7222cda541602d2e23e
william <william-cullum@hotmail.co.uk>
2025-02-19 20:31:38 +0000

    got mapi working atm in some janky way

--- MetOfficeAPI/main.go
+++ MetOfficeAPI/main.go
@@ -14,9 +14,7 @@ import (
 )
 
 func main() {
-	log := NewLogger("log.txt")
-
-	log.Println("hello from the log :)")
+	logger := NewLogger("log.txt")
 
 	if err := godotenv.Load(".env"); err != nil {
 		log.Fatalf("couldn't load environment variables")
@@ -26,20 +24,15 @@ func main() {
 		Addr:     "redis-17171.c338.eu-west-2-1.ec2.redns.redis-cloud.com:17171",
 		Password: os.Getenv("redis"),
 		DB:       0,
-		Username: "default",
 	})
 
-	api := NewMetAPI(os.Getenv("apikey"), log)
-	store := NewMetStore(rc, log)
+	api := NewMetAPI(os.Getenv("apikey"), logger)
+	store := NewMetStore(rc, logger)
 
 	ctx := context.Background()
 	errs := make(chan error, 1)
-	log.Print(store.GetLastUpdate())
-
-	// temp use o
-	store.SetLastUpdatedNow()
-
-	go UpdateForecasts(ctx, store.GetLastUpdate(), api, store, errs)
+	logger.Print(time.Since(store.GetLastUpdate()))
+	go UpdateForecasts(logger, ctx, store.GetLastUpdate(), api, store, errs)
 
 	// not sure if we want to handle our error this way but before we were blocking the whole app waiting
 	// for error that maybe never occured
@@ -55,10 +48,14 @@ func main() {
 	}()
 
 	// srv := NewServer(log, store)
-	// log.Println("Starting server on 8181")
-	// if err := http.ListenAndServe(":8181", srv.mux); err != nil {
+	// if err := http.ListenAndServe(":8181", srv); err != nil {
 	// 	panic("failed starting server")
 	// }
+
+	// untill we have the server running again we are just using this so that we can keep main running
+
+	select {}
+
 }
 
 func Str2Time(timeString string) (time.Time, error) {
@@ -71,7 +68,8 @@ func Str2Time(timeString string) (time.Time, error) {
 }
 
 // this is not correct. it needs to be updating on the hour, not an hour from when the prog was run
-func UpdateForecasts(ctx context.Context, lastUpdate time.Time, api MetAPI, store *MetStore, errs chan<- error) error {
+
+func UpdateForecasts(log *log.Logger, ctx context.Context, lastUpdate time.Time, api MetAPI, store *MetStore, errs chan<- error) error {
 
 	log.Print("Starting forecast update process")
 
@@ -158,18 +156,18 @@ func UpdateForecasts(ctx context.Context, lastUpdate time.Time, api MetAPI, stor
 
 var crags = []Crag{
 	{"cromlech", 53.08977582752912, -4.0494354521953895},
-	// {"beddgelert", 53.01401346937128, -4.1086367318613055},
-	// {"gwynant", 53.04567339439013, -4.021447439922229},
-	// {"blaenau", 52.99729599359651, -3.9578734953238475},
-	// {"crafnant", 52.99729599359651, -3.9578734953238475},
-	// {"cwellyn", 53.07568570139747, -4.148701296939546},
-	// {"orme", 53.33236585445307, -3.8311890286450865},
-	// {"Penmaenbach", 53.285, -3.8684},
-	// {"ysgo", 52.80614677538971, -4.656639551730091},
-	// {"tremadoch", 52.94008535336955, -4.140997768369204},
-	// {"rhiwGoch", 53.09199013529737, -3.803795346023221},
-	// {"portland", 50.545900401402854, -2.438814867485551},
-	// {"cuckooRock", 50.545900401402854, -2.438814867485551},
-	// {"MountSionEast", 50.545900401402854, -2.438814867485551},
-	// {"Froggatt", 53.2942103060766, -1.6201285054945418},
+	{"beddgelert", 53.01401346937128, -4.1086367318613055},
+	{"gwynant", 53.04567339439013, -4.021447439922229},
+	{"blaenau", 52.99729599359651, -3.9578734953238475},
+	{"crafnant", 52.99729599359651, -3.9578734953238475},
+	{"cwellyn", 53.07568570139747, -4.148701296939546},
+	{"orme", 53.33236585445307, -3.8311890286450865},
+	{"Penmaenbach", 53.285, -3.8684},
+	{"ysgo", 52.80614677538971, -4.656639551730091},
+	{"tremadoch", 52.94008535336955, -4.140997768369204},
+	{"rhiwGoch", 53.09199013529737, -3.803795346023221},
+	{"portland", 50.545900401402854, -2.438814867485551},
+	{"cuckooRock", 50.545900401402854, -2.438814867485551},
+	{"MountSionEast", 50.545900401402854, -2.438814867485551},
+	{"Froggatt", 53.2942103060766, -1.6201285054945418},
 }
